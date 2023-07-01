85bdf33099bba31a66002ad46103b0a8f29afa9f
Carlos Veras <pessoal.carlosveras@gmail.com>
2023-07-01 18:03:28 -0300

    resolvendo conflito

--- src/App.java
+++ src/App.java
@@ -6,7 +6,10 @@ public class App {
 	public static void main(String args[]) {
 		Atracao sorveteria = new Atracao("Sorveteria", "Bom Sabor", 001, 30, 0, 0);
 		Visitante Max = new Visitante("Maxwell", 100);
+
 		System.out.println("Canja");
+		
+		System.out.println("Você é demais!");
 		
 		sorveteria.receberVisitante(Max);
 		Max.mostrarAtracoes();
