002599e620e8964ddc108be335bccb35c5ea6815
Leandro Iglesias <lelouchiglesias@gmail.com>
2023-07-01 15:27:57 -0300

    Testacanja

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
 	public static void main(String args[]) {
 		Atracao sorveteria = new Atracao("Sorveteria", "Bom Sabor", 001, 30, 0, 0);
 		Visitante Max = new Visitante("Maxwell", 100);
-		
+		System.out.println("Canja");
 		
 		sorveteria.receberVisitante(Max);
 		Max.mostrarAtracoes();
